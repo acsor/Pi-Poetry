18636b11178a7ba3bb203809cf3ea4867d041529
n0ne <new.new.none@gmail.com>
2017-01-06 15:45:58 +0100

    Implement points a. and b. of Problem 1.

--- src/piwords/PiGenerator.java
+++ src/piwords/PiGenerator.java
@@ -18,7 +18,7 @@ public class PiGenerator {
     }
 
     /**
-     * Computes a^b mod m
+     * Computes a ^ b mod m
      * <p>
      * If a < 0, b < 0, or m < 0, return -1.
      *
@@ -28,8 +28,46 @@ public class PiGenerator {
      * @return a^b mod m
      */
     public static int powerMod (int a, int b, int m) {
-        // TODO: Implement (Problem 1.b)
-        return -1;
+        /*
+        let binB equal to the binary expansion of b (the exponent)
+        iterate through binB with index variable i
+        when binB[i] == 1, we can:
+            calculate the power with base base and exponent 2 ^ i
+            calculate mod = power % mod
+            multiply mod to the result
+         */
+        if (a < 0 || b < 0 || m < 0) {
+            return -1;
+        } else if (a == 0) {
+            return m;
+        }
+
+        final char[] binaryExpansion = Integer.toBinaryString(b).toCharArray();
+        int result = 1;
+        int power;
+
+        for (int i = binaryExpansion.length - 1; i >= 0; i--) {
+            if (Integer.parseInt(String.valueOf(binaryExpansion[i])) == 1) {
+                power = calculatePower(a, i, binaryExpansion);
+
+                if (result == 0) {
+                    result = power % m;
+                } else {
+                    result *= power % m;
+                }
+            }
+        }
+
+        return result % m;
+    }
+
+    private static int calculatePower (int base, int index, char[] binaryString) {
+        final int realIndex = binaryString.length - 1 - index;
+
+        return (int) Math.pow (
+                base,
+                Math.pow(2, realIndex)
+        );
     }
 
     /**
--- src/piwords/PiGeneratorTest.java
+++ src/piwords/PiGeneratorTest.java
@@ -1,15 +1,90 @@
 package piwords;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+
 public class PiGeneratorTest {
+
+    private static final int CONST_MAX_BASE = 10;
+    private static final int CONST_MAX_EXP = 9;
+    private static final int CONST_MAX_MOD = 20;
+
+    private static final int CONST_DEFAULT_MOD = 7;
+
+    private Random randomGen;
+
+    public PiGeneratorTest () {
+        randomGen = new Random();
+    }
+
     @Test
-    public void basicPowerModTest() {
+    public void basicPowerModTest () {
         // 5^7 mod 23 = 17
         assertEquals(17, PiGenerator.powerMod(5, 7, 23));
     }
 
-    // TODO: Write more tests (Problem 1.a, 1.c)
+    @Test
+    public void powerModTest () {
+        final int[] bases = randomGen.ints(4, 1, CONST_MAX_BASE).toArray();
+        final int minExponent = 0;
+        final int modulo = randomGen.nextInt(CONST_MAX_MOD);
+
+        for (int exp = minExponent; exp <= CONST_MAX_EXP; exp++) {
+            for (int base: bases) {
+                // System.out.format("%d ^ %d mod %d = %d\n", base, exp, modulo, (int) Math.pow(base, exp) % modulo);
+                // System.out.format("PiGenerator.powerMod(%d, %d, %d) = %d\n\n", base, exp, modulo, PiGenerator.powerMod(base, exp, modulo));
+                Assert.assertEquals(
+                        Math.pow(base, exp) % modulo,
+                        PiGenerator.powerMod(base, exp, modulo),
+                        0
+                );
+            }
+        }
+    }
+
+    @Test
+    public void powerModTestTemp () {
+        final int base = 9, exp = 15, modulo = 13;
+
+        Assert.assertEquals(
+                Math.pow(base, exp) % modulo,
+                PiGenerator.powerMod(base, exp, modulo),
+                0
+        );
+    }
+
+    @Test
+    public void powerModTestZeroExponent () {
+        final int
+        base = randomGen.nextInt(CONST_MAX_BASE),
+        exp = 0,
+        mod = CONST_DEFAULT_MOD;
+
+        Assert.assertEquals(
+                Math.pow(base, exp) % mod,
+                PiGenerator.powerMod(base, exp, mod),
+                0
+        );
+    }
+
+    @Test
+    public void powerModTestZeroModulo () {
+        final int
+                base = randomGen.nextInt(CONST_MAX_BASE),
+                exp = randomGen.nextInt(CONST_MAX_EXP),
+                mod = CONST_DEFAULT_MOD;
+
+        Assert.assertEquals (
+                Math.pow(base, exp) % mod,
+                PiGenerator.powerMod(base, exp, mod),
+                0
+        );
+    }
+
+    // TO-DO: Write more tests (Problem 1.a, 1.c)
+
 }
