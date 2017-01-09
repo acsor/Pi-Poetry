50b011a5ea0a4a3562711af801b827245abdf675
n0ne <new.new.none@gmail.com>
2017-01-09 12:13:27 +0100

    Complete Problem 2. Correctness of convertBase() not ensured, though it passes the default test and boundary conditions tests.

--- src/piwords/BaseTranslator.java
+++ src/piwords/BaseTranslator.java
@@ -3,27 +3,39 @@ package piwords;
 public class BaseTranslator {
 
     /**
-     * Converts an array where the ith digit corresponds to (1 / baseA)^(i + 1)
+     * <p>
+     * Converts an array where the ith digit corresponds to (1 / baseA) ^ (i + 1)
      * digits[i], return an array output of size precisionB where the ith digit
-     * corresponds to (1 / baseB)^(i + 1) * output[i].
+     * corresponds to (1 / baseB) ^ (i + 1) * output[i].
+     * </p>
      * <p>
      * Stated in another way, digits is the fractional part of a number
      * expressed in baseA with the most significant digit first. The output is
      * the same number expressed in baseB with the most significant digit first.
+     * </p>
      * <p>
      * To implement, logically, you're repeatedly multiplying the number by
      * baseB and chopping off the most significant digit at each iteration:
+     * </p>
      * <p>
      * for (i < precisionB) {
-     *      1. Keep a carry, initialize to 0.
-     *      2. From RIGHT to LEFT
-     *      a. x = multiply the ith digit by baseB and add the carry
-     *      b. the new ith digit is x % baseA
-     *      c. carry = x / baseA
-     *      3. output[i] = carry
+     * <ol>
+     * <li>Keep a carry, initialize to 0.</li>
+     * <li>
+     * From RIGHT to LEFT
+     * <ol>
+     * <li>x = multiply the ith digit by baseB and add the carry</li>
+     * <li>the new ith digit is x % baseA</li>
+     * <li>carry = x / baseA</li>
+     * </ol>
+     * </li>
+     * <li>output[i] = carry</li>
+     * </ol>
+     * </p>
      * <p>
-     * If digits[i] < 0 or digits[i] >= baseA for any i, return null
-     * If baseA < 2, baseB < 2, or precisionB < 1, return null
+     * If digits[i] < 0 or digits[i] >= baseA for any i, return null.<br>
+     * If baseA < 2, baseB < 2, or precisionB < 1, return null.
+     * </p>
      *
      * @param digits     The input array to translate. This array is not mutated.
      * @param baseA      The base that the input array is expressed in.
@@ -33,8 +45,30 @@ public class BaseTranslator {
      * @return An array of size precisionB expressing digits in baseB.
      */
     public static int[] convertBase (int[] digits, int baseA, int baseB, int precisionB) {
-        // TODO: Implement (Problem 2.b)
-        return null;
+        final int[] output = new int[precisionB];
+        int carry, x;
+
+        if (baseA < 2 || baseB < 2 || precisionB < 1) {
+            return null;
+        }
+
+        for (int i = 0; i < precisionB; i++) {
+            carry = 0;
+
+            for (int j = digits.length - 1; j >= 0; j--) {
+                if (digits[j] < 0 || digits[j] >= baseA) {
+                    return null;
+                }
+
+                x = (digits[j] * baseB) + carry;
+                digits[j] = x % baseA;
+                carry = x / baseA;
+            }
+
+            output[i] = carry;
+        }
+
+        return output;
     }
 
 }
--- src/piwords/BaseTranslatorTest.java
+++ src/piwords/BaseTranslatorTest.java
@@ -1,11 +1,13 @@
 package piwords;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Random;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class BaseTranslatorTest {
 
@@ -32,17 +34,75 @@ public class BaseTranslatorTest {
     }
 
     @Test
+    @Ignore
+    public void convertBaseTenToBin () {
+        final int[][] baseTenInput = {
+                {4, 9},
+                {5, 5},
+                {1, 6},
+                {3, 2}
+        };
+        final int[][] baseTwoOutput = {
+                {1, 1, 0, 0, 0, 1},
+                {1, 1, 0, 1, 1, 1},
+                {1, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0}
+        };
+
+        for (int i = 0; i < Math.min(baseTenInput.length, baseTwoOutput.length); i++) {
+            assertArrayEquals(
+                    baseTwoOutput[i],
+                    BaseTranslator.convertBase(
+                            baseTenInput[i],
+                            10,
+                            2,
+                            baseTwoOutput[i].length
+                    )
+            );
+        }
+    }
+
+    @Test
+    @Ignore
+    public void convertBaseBinToTen () {
+        final int[][] baseTwoInput = {
+                {1, 1, 0, 0, 0, 1},
+                {1, 1, 0, 1, 1, 1},
+                {1, 0, 0, 0, 0},
+                {1, 0, 0, 0, 0, 0}
+        };
+        final int[][] baseTenOutput = {
+                {4, 9},
+                {5, 5},
+                {1, 6},
+                {3, 2}
+        };
+
+        for (int i = 0; i < Math.min(baseTwoInput.length, baseTenOutput.length); i++) {
+            assertArrayEquals(
+                    baseTenOutput[i],
+                    BaseTranslator.convertBase(
+                            baseTwoInput[i],
+                            2,
+                            10,
+                            baseTenOutput[i].length
+                    )
+            );
+        }
+    }
+
+    @Test
     public void convertBaseNegativeDigits () {
         final int[] input = {0, 3, 4, -1, -10};
 
         assertEquals(
+                null,
                 BaseTranslator.convertBase(
                         input,
                         VAL_DEFAULT_INPUT_BASE,
                         VAL_DEFAULT_OUTPUT_BASE,
                         input.length / 2
-                ),
-                null
+                )
         );
     }
 
@@ -51,13 +111,13 @@ public class BaseTranslatorTest {
         final int[] input = {3, 4, 16, 9, 20, 14};
 
         assertEquals(
+                null,
                 BaseTranslator.convertBase(
                         input,
                         VAL_DEFAULT_INPUT_BASE,
                         VAL_DEFAULT_OUTPUT_BASE,
                         input.length / 2
-                ),
-                null
+                )
         );
     }
 
@@ -104,6 +164,25 @@ public class BaseTranslatorTest {
                 ),
                 null
         );
+    }
+
+    @Test
+    @Ignore
+    public void testArray () {
+        final int[] a = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        final int[] b = a.clone();
+
+        printIntArray(a);
+        printIntArray(b);
+
+        assertTrue(a != b);
+    }
+
+    private void printIntArray (final int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.format("%d|", array[i]);
+        }
+        System.out.println();
     }
 
     // TODO: Write more tests (Problem 2.a)
