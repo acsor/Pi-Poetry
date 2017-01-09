013b4494cd8cbfd9573121b1e8d5cfdcf9fe709e
n0ne <new.new.none@gmail.com>
2017-01-09 15:55:56 +0100

    Complete Problem 3.

--- src/piwords/DigitsToStringConverter.java
+++ src/piwords/DigitsToStringConverter.java
@@ -1,24 +1,40 @@
 package piwords;
 
 public class DigitsToStringConverter {
+
     /**
+     * <p>
      * Given a list of digits, a base, and an mapping of digits of that base to
      * chars, convert the list of digits into a character string by applying the
      * mapping to each digit in the input.
-     * 
+     * </p>
+     * <p>
      * If digits[i] >= base or digits[i] < 0 for any i, consider the input
-     * invalid, and return null.
+     * invalid, and return null.<br>
      * If alphabet.length != base, consider the input invalid, and return null.
+     * </p>
      *
-     * @param digits A list of digits to encode. This object is not mutated.
-     * @param base The base the digits are encoded in.
+     * @param digits   A list of digits to encode. This object is not mutated.
+     * @param base     The base the digits are encoded in.
      * @param alphabet The mapping of digits to chars. This object is not
      *                 mutated.
      * @return A String encoding the input digits with alphabet.
      */
-    public static String convertDigitsToString(int[] digits, int base,
-                                               char[] alphabet) {
-        // TODO: Implement (Problem 3.b)
-        return "";
+    public static String convertDigitsToString (int[] digits, int base, char[] alphabet) {
+        if (alphabet.length != base) {
+            return null;
+        }
+
+        final StringBuilder sb = new StringBuilder(digits.length);
+
+        for (int i: digits) {
+            if (i < 0 || i >= base) {
+                return null;
+            }
+            sb.append(alphabet[i]);
+        }
+
+        return sb.toString();
     }
+
 }
--- src/piwords/DigitsToStringConverterTest.java
+++ src/piwords/DigitsToStringConverterTest.java
@@ -1,23 +1,53 @@
 package piwords;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class DigitsToStringConverterTest {
+
     @Test
-    public void basicNumberSerializerTest() {
-        // Input is a 4 digit number, 0.123 represented in base 4
-        int[] input = {0, 1, 2, 3};
+    public void basicNumberSerializerTest () {
+        int[] input = {0, 1, 2, 3}; // Input is a 4 digit number, 0.123 represented in base 4
+        char[] alphabet = {'d', 'c', 'b', 'a'}; // Want to map 0 -> "d", 1 -> "c", 2 -> "b", 3 -> "a"
+        final String expectedOutput = "dcba";
 
-        // Want to map 0 -> "d", 1 -> "c", 2 -> "b", 3 -> "a"
-        char[] alphabet = {'d', 'c', 'b', 'a'};
+        assertEquals(
+                expectedOutput,
+                DigitsToStringConverter.convertDigitsToString(input, 4, alphabet)
+        );
+    }
 
-        String expectedOutput = "dcba";
-        assertEquals(expectedOutput,
-                     DigitsToStringConverter.convertDigitsToString(
-                             input, 4, alphabet));
+    @Test
+    public void testDigitsNegative () {
+        int[] input = {10, -10, 4, -949};
+        char[] alphabet = "abcdefghijk".toCharArray();
+
+        assertEquals(
+                null,
+                DigitsToStringConverter.convertDigitsToString(
+                        input,
+                        alphabet.length,
+                        alphabet
+                )
+        );
+    }
+
+    @Test
+    public void testDigitsOverflow () {
+        int[] input = {10, 10, 4, 11};
+        char[] alphabet = "abcdefghijk".toCharArray();
+
+        assertEquals (
+                null,
+                DigitsToStringConverter.convertDigitsToString(
+                        input,
+                        alphabet.length,
+                        alphabet
+                )
+        );
     }
 
     // TODO: Write more tests (Problem 3.a)
+
 }
